a0e4e859476526220b55dbd95dbcc33bc480a50c
omar ibi <omaribi@MacBook-Air.local>
2019-12-17 18:15:17 -0500

    modified for change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "Her" });
+  res.send({ hi: "Herr" });
 });
 
 const PORT = process.env.PORT || 5000;
