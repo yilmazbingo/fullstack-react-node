34356730e24116fcd8792b0eb41a1bbdb5ade362
omar ibi <omaribi@MacBook-Air.local>
2020-01-20 08:21:35 -0500

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,8 @@ import reduxThunk from "redux-thunk";
 import logger from "redux-logger";
 
 import * as serviceWorker from "./serviceWorker";
-
+import axios from "axios";
+window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk, logger));
 
 ReactDOM.render(
